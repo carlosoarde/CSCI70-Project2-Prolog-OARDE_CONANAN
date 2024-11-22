c80e6a185d214088cd001710ac84257b3b9271bf
carlosoarde <cgroarde@gmail.com>
2024-11-22 13:12:03 +0800

    Version 1.1: Added comments  and “ ,!. ”  to some queries

--- firm2.pl
+++ firm2.pl
@@ -8,10 +8,10 @@ head_of(marketing, mike).
 
 % Busy Department Rule
 busy_department(Dept) :-
-    department(Dept),
-    \+ (belongs_to(Emp, Dept), \+ assigned_to(Emp, _)).
+    department(Dept), % Check if Dept is a valid department
+    \+ (belongs_to(Emp, Dept), \+ assigned_to(Emp, _)). % Ensure all employees in Dept are assigned to at least one project
 
 % Efficient Employee Rule
 efficient_employee(Emp) :-
-    assigned_to(Emp, Proj),
-    forall(assigned_to(Emp, P), status(P, completed)).
+    assigned_to(Emp, _), % Ensure the employee is assigned to at least one project
+    forall(assigned_to(Emp, P), status(P, completed)). % Check that all assigned projects are completed
